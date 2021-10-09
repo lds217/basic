30207314f6b89efbebed2a87d4e17f29ac7f4cce
ldss13 <lds050606@gmail.com>
2021-10-09 09:10:51 +0700

    Create BASIC3.cpp

---
+++ BASIC3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;int j;
+    cin>>n;
+    int count=n;
+    for(int i=0;i<n;i++)
+    {
+        for(j=1;j<count;j++){
+            cout<<"*";
+        }
+        if(j==count)
+            cout<<"*"<<endl;
+    count--;}
+    system("pause");
+	return(0);
+}
