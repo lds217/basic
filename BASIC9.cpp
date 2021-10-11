1ac1965dd5134355bf267b5b2845f5dc8b27d71a
ldss13 <lds050606@gmail.com>
2021-10-11 17:15:15 +0700

    Update BASIC9.cpp

--- BASIC9.cpp
+++ BASIC9.cpp
@@ -3,18 +3,21 @@ using namespace std;
 
 int main()
 {
-    int n;int j;int k;
-    cin>>n;
-    int count=n;
-    for(int i=0;i<n;i++)
-    { for(k=count;k<n;k++) 
+int n;int j;int k;
+cin>>n;
+int count=n;
+for(int i=0;i<n;i++)
+{ 
+    for(k=count;k<n;k++) 
         cout<<" ";
-    if(k==n) {
+    if(k==n) 
+    {
         for(int j=0;j<count;j++)
-            cout<<"x";}
-    cout<<endl;
-    count--;
+            cout<<"x";
     }
-    system("pause");
-	return(0);
+cout<<endl;
+count--;
+}
+system("pause");
+return(0);
 }
