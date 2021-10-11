257ad439853b26ca7d7e4da53f185e0ec52458ca
ldss13 <lds050606@gmail.com>
2021-10-11 17:13:04 +0700

    Create BASIC9.cpp

---
+++ BASIC9.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;int j;int k;
+    cin>>n;
+    int count=n;
+    for(int i=0;i<n;i++)
+    { for(k=count;k<n;k++) 
+        cout<<" ";
+    if(k==n) {
+        for(int j=0;j<count;j++)
+            cout<<"x";}
+    cout<<endl;
+    count--;
+    }
+    system("pause");
+	return(0);
+}
