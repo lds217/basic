405c9beb6f1d3983d5705480c80b7871b2d8ebe1
ldss13 <lds050606@gmail.com>
2021-10-11 10:18:31 +0700

    Create BASIC8.cpp

---
+++ BASIC8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   int m; int n;
+    cin>>m;
+    cin>>n;
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+            {cout<<"*";}
+    cout<<endl;
+    }
+    system("pause");
+	return(0);
+}
