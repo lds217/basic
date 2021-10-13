27495c28d786631b5e5ec81204114cd68a63466a
ldss13 <lds050606@gmail.com>
2021-10-13 11:16:21 +0700

    Update BASIC19.cpp

--- BASIC19.cpp
+++ BASIC19.cpp
@@ -2,16 +2,22 @@
 using namespace std;
 
 int main()
-{   int n;
+{   int n;int i;
     cout<<" nhap so n ";
     cin>>n;
-    int temp=n;
-    for(int i=n;i>0;i--)
+    int temp=1;
+    int count=n;
+    for(i=1;i<=n;i++)
     {
-        for(int j=temp;j>0;j--)
-            cout<<i;
+        for(int k=1;k<=n;k++)
+           {if(i==k)
+                for(int j=n;j>=k;j--)
+                    cout<<count;   
+            else
+                cout<<" ";}   
         cout<<endl;
-        temp--;
+        count--;
+        temp++;
     }
 system("pause");
 return(0);
