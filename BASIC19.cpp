3e3c61b402cee246047fb3de7414d4b613e4bd7d
ldss13 <lds050606@gmail.com>
2021-10-13 12:42:25 +0700

    Update BASIC19.cpp

--- BASIC19.cpp
+++ BASIC19.cpp
@@ -5,16 +5,12 @@ int main()
 {   int n;int i;
     cout<<" nhap so n ";
     cin>>n;
-    int temp=1;
+    int temp=0;
     int count=n;
     for(i=1;i<=n;i++)
     {
-        for(int k=1;k<=n;k++)
-           {if(i==k)
-                for(int j=n;j>=k;j--)
-                    cout<<count;   
-            else
-                cout<<" ";}   
+        for(int k=temp;k<n;k++)
+            cout<<count;   
         cout<<endl;
         count--;
         temp++;
